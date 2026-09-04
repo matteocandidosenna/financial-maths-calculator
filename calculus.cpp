cf40689c81d4bc35d70c92fc569719d3f0decb7e
Matheus Candido <matteocandidosenna@gmail.com>
2026-09-04 10:26:31 -0300

    general functions

--- calculus.cpp
+++ calculus.cpp
@@ -1,8 +1,32 @@
+/*A fórmula básica para calcular o valor dos juros simples é J = C × i × t. [1] 
+O Significado de Cada LetraJ: Juros (o valor total que será pago ou recebido).
+C: Capital (o valor inicial emprestado ou investido).
+i: Taxa de juros (o percentual cobrado por período, usado em formato decimal, como 5% = 0,05).
+t: Tempo (o período em que o dinheiro fica aplicado ou emprestado).*/
+
 #include <bits/stdc++.h>
+#include <calculus.hpp>
 using namespace std;
 
 //juros simples
-double converter_tempo(int anos, int meses, int dias){
-    //a unidade base vai ser anos
+double converter_tempo(struct tempo periodo, double taxa){
+    //a unidade base vai ser meses
+    double tempo_convertido = 0.0;
+    return tempo_convertido;
+}
+
+double valor_final(){
     
+}
+
+double taxa(){
+
+}
+
+double tempo(){
+
+}
+
+double valor_presente(){
+
 }
--- calculus.hpp
+++ calculus.hpp
@@ -1,2 +1,20 @@
+/*A fórmula básica para calcular o valor dos juros simples é J = C × i × t. [1] 
+O Significado de Cada LetraJ: Juros (o valor total que será pago ou recebido).
+C: Capital (o valor inicial emprestado ou investido).
+i: Taxa de juros (o percentual cobrado por período, usado em formato decimal, como 5% = 0,05).
+t: Tempo (o período em que o dinheiro fica aplicado ou emprestado).*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
+typedef struct tempo{
+    int dias;
+    int meses; 
+    int anos;
+} tempo;
+
+double converter_tempo(struct tempo periodo, double taxa);
+double valor_final(double valor_inicial, double taxa);
+double taxa(double valor_inicial, double valor_final, struct tempo perido);
+double tempo(double valor_inicial, double valor_final, double taxa);
+double valor_presente(double valor_final, struct tempo periodo);
